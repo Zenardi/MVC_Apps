f3ff717c3b6980d23ad974ad16a81bc89392b0ab
Eduardo Doval Zenardi Filho <du.zenardi@gmail.com>
2017-02-21 20:36:33 -0300

    Add more fields

--- VidlyProject/Vidly/Controllers/CustomerController.cs
+++ VidlyProject/Vidly/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
@@ -12,40 +13,35 @@ namespace Vidly.Controllers
 
         public CustomerController()
         {
-            //_context = new ApplicationDbContext();
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult New()
+        {
+            return View();
         }
 
         protected override void Dispose(bool disposing)
         {
-            //_context.Dispose();
+            _context.Dispose();
         }
 
         public ViewResult Index()
         {
-            //var customers = _context.Customers.ToList();
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(String id)
         {
-            //var customer =_context.Customers.SingleOrDefault(c => c.Id == id);
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer =_context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = "1", Name = "John Smith" },
-                new Customer { Id = "2", Name = "Mary Williams" }
-            };
-        }
 
     }
 }
