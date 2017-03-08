f655bba47b27166e0638084fe62163993d02ffe3
Eduardo Doval Zenardi Filho <du.zenardi@gmail.com>
2017-03-08 10:44:15 -0300

    Implemented WebAPI using automapper

---
+++ Blockbuster/Blockbuster/App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Blockbuster.Dtos;
+using Blockbuster.Models;
+
+namespace Blockbuster.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Maps based on their variables names
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+
+        }
+    }
+}
---
+++ Blockbuster/Blockbuster/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Blockbuster.Models;
+
+namespace Blockbuster.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter customer name")]
+        [StringLength(255)]
+        public String Name { get; set; }
+
+        public bool IsSubscribedNewsLetter { get; set; }
+        
+        
+        public byte MembershipTypesId { get; set; }
+        
+        [Min18YearsIfMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
